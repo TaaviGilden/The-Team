43c4ac24e30c9d7fab6fbd03131f4701b971f9f7
kmetsand <kmetsand@ut.ee>
2013-10-11 10:43:59 +0300

    fifth commit to memory

--- POS/src/ee/ut/math/tvt/TheTeam/IntroUI.java
+++ POS/src/ee/ut/math/tvt/TheTeam/IntroUI.java
@@ -4,56 +4,78 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class IntroUI extends JFrame {
-	
+
 	private static final long serialVersionUID = 1L;
 
-	public static void iUI(){
-		
-		JFrame raam=new JFrame();
-		raam.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		raam.setSize(500, 300);
-		raam.setResizable(false);
-		raam.setTitle("Intro");
-		raam.setContentPane(new JLabel(new ImageIcon("background.png")));
-		
-		
-		JLabel teamName = new JLabel("Team name: The Team");
+	public static void iUI() {
+
+		Properties app = new Properties();
+		Properties ver = new Properties();
+
+		try {
+
+			app.load(new FileInputStream("application.properties"));
+			ver.load(new FileInputStream("version.properties"));
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(500, 300);
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		frame.setLocation(dim.width / 2 - frame.getSize().width / 2, dim.height
+				/ 2 - frame.getSize().height / 2);
+		frame.setResizable(false);
+		frame.setTitle("Intro");
+		frame.getContentPane().setBackground(Color.WHITE);
+
+		JLabel teamName = new JLabel("Team name: "
+				+ app.getProperty("TeamName"));
 		teamName.setBounds(1, 10, 200, 30);
-		raam.add(teamName);
-		
-		
-		JLabel teamLeader = new JLabel("Team Leader: Taavi Gilden");
+		frame.add(teamName);
+
+		JLabel teamLeader = new JLabel("Team Leader: "
+				+ app.getProperty("TeamLeader"));
 		teamLeader.setBounds(1, 40, 200, 30);
-		raam.add(teamLeader);
-		
-		
-		JLabel teamLeaderMail = new JLabel("Team leader email: Taavi.Gilden@gmail.com");
+		frame.add(teamLeader);
+
+		JLabel teamLeaderMail = new JLabel("Team leader email: "
+				+ app.getProperty("TeamLeaderEmail"));
 		teamLeaderMail.setBounds(1, 70, 350, 30);
-		raam.add(teamLeaderMail);
-		
-		
-		JLabel teamMembers = new JLabel("Team members: Taavi Gilden, Karl Metsand, Kristiina Ilves, Peeter Robert Reissar");
+		frame.add(teamLeaderMail);
+
+		JLabel teamMembers = new JLabel("Team members: "
+				+ app.getProperty("TeamMembers"));
 		teamMembers.setBounds(1, 100, 500, 30);
-		raam.add(teamMembers);
-		
-		
-		
-		JLabel version = new JLabel("Build.number: 0.0.1");
+		frame.add(teamMembers);
+
+		JLabel version = new JLabel("Version: "
+				+ ver.getProperty("build.number"));
 		version.setBounds(1, 130, 200, 30);
-		raam.add(version);
-		
+		frame.add(version);
+
 		ImageIcon pilt = new ImageIcon("logo.png");
 		JLabel logo = new JLabel(pilt);
-		logo.setBounds(380,160,100,100);
-		raam.add(logo);
-		
-		//Miskipärast ei töötanud setBounds meetod viimasel lisatud JLabelil ning seega lisasin tühja JLabeli
+		logo.setBounds(380, 160, 100, 100);
+		frame.add(logo);
+
+		// Miskipärast ei töötanud setBounds meetod viimasel lisatud JLabelil
+		// ning seega lisasin tühja JLabeli
 		JLabel empty = new JLabel("");
-		empty.setBounds(0,150,0,0);
-		raam.add(empty);
-		
-		
-		raam.setVisible(true);
+		empty.setBounds(0, 150, 0, 0);
+		frame.add(empty);
+
+		frame.setVisible(true);
+
 	}
 }
