20b9073d9a991ecf1ce4f4f1c5ae07cedb49b363
kmetsand <kmetsand@ut.ee>
2013-11-24 20:27:41 +0200

    PurchaseInfoTableModelTest.

---
+++ task1/test/ee/ut/math/tvt/PurchaseInfoTableModelTest.java
@@ -0,0 +1,34 @@
+package ee.ut.math.tvt;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ee.ut.math.tvt.salessystem.domain.data.SoldItem;
+import ee.ut.math.tvt.salessystem.domain.data.StockItem;
+import ee.ut.math.tvt.salessystem.ui.model.PurchaseInfoTableModel;
+
+public class PurchaseInfoTableModelTest {
+	private StockItem stockItem1;
+	private SoldItem soldItem1;
+	PurchaseInfoTableModel purchaseInfoTableModel;
+
+	@Before
+	public void setUp() {
+		stockItem1 = new StockItem(1L, "Alexander", "Õlu", 2.0, 5);
+		soldItem1 = new SoldItem(stockItem1, 5);
+		soldItem1.setId(1L);
+		purchaseInfoTableModel = new PurchaseInfoTableModel();
+
+	}
+
+	@Test
+	public void testAddItem() {
+
+		purchaseInfoTableModel.addItem(soldItem1);
+
+		assertEquals(soldItem1, purchaseInfoTableModel.getItemById(1));
+	}
+
+}
