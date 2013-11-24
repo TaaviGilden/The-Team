256bcb64123b5d75f4dd9cb145b7039161b5afbb
kmetsand <kmetsand@ut.ee>
2013-11-24 12:38:44 +0200

    Added task2.

--- POS/src/ee/ut/math/tvt/salessystem/domain/controller/impl/SalesDomainControllerImpl.java
+++ POS/src/ee/ut/math/tvt/salessystem/domain/controller/impl/SalesDomainControllerImpl.java
@@ -53,17 +53,6 @@ public class SalesDomainControllerImpl implements SalesDomainController {
 	public List<StockItem> loadWarehouseState() {
 		// XXX mock implementation
 		return (List<StockItem>)(session.createQuery("from StockItem").list());
-//		StockItem chips = new StockItem(1l, "Lays chips", "Potato chips", 11.0, 5);
-//		StockItem chupaChups = new StockItem(2l, "Chupa-chups", "Sweets", 8.0, 8);
-//	    StockItem frankfurters = new StockItem(3l, "Frankfurters", "Beer sauseges", 15.0, 12);
-//	    StockItem beer = new StockItem(4l, "Free Beer", "Student's delight", 0.0, 100);
-//
-//		dataset.add(chips);
-//		dataset.add(chupaChups);
-//		dataset.add(frankfurters);
-//		dataset.add(beer);
-//		
-//		return dataset;
 	}
 	
 	public void endSession() {
